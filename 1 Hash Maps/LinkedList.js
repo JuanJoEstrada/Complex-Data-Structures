bcee10ba8f52ab743d4f4a3fbc1e4d08fbb577cf
JuanEstrada <juan_estrada_1@hotmail.com>
2021-07-21 19:02:48 -0500

    replacing LinkedList

--- 1 Hash Maps/LinkedList.js
+++ 1 Hash Maps/LinkedList.js
@@ -17,7 +17,7 @@ class Node {
 }
 
 class LinkedList {
-
+  
   constructor() {
     this.head = null;
   }
@@ -26,41 +26,64 @@ class LinkedList {
     const newHead = new Node(data);
     const currentHead = this.head;
     this.head = newHead;
-    if (currentHead) newHead.setNextNode(currentHead);
+    if (currentHead) {
+      this.head.setNextNode(currentHead);
+    }
   }
 
   addToTail(data) {
     let tail = this.head;
     if (!tail) {
-
       this.head = new Node(data);
-
     } else {
-
       while (tail.getNextNode() !== null) {
         tail = tail.getNextNode();
       }
       tail.setNextNode(new Node(data));
-
     }
   }
 
   removeHead() {
     const removedHead = this.head;
-    if (!removedHead) return null;
-    this.head = removedHead.getNextNode();
+    if (!removedHead) {
+      return;
+    }
+    if (removedHead.next) {
+      this.head = removedHead.next;
+    }
     return removedHead.data;
   }
 
   printList() {
     let currentNode = this.head;
-    let outPut = '<head> ';
+    let output = '<head> ';
     while (currentNode !== null) {
-      outPut += `${currentNode.data} `;
-      currentNode = currentNode.getNextNode();
+      output += currentNode.data + ' ';
+      currentNode = currentNode.next;
     }
-    outPut += '<tail>';
-    console.log(outPut);
+    output += `<tail>`;
+    console.log(output);
+  }
+
+  findNodeIteratively(data) {
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.data === data) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  findNodeRecursively(data, currentNode = this.head) {
+    if (currentNode === null) {
+      return null;
+    } else if (currentNode.data === data) {
+      return currentNode;
+    } else {
+      return this.findNodeRecursively(data, currentNode.next);
+    }
   }
 
 }
