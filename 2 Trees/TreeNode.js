34eac5eb877c5d43765adfa5c2465d37cd82ce06
JuanEstrada <juan_estrada_1@hotmail.com>
2021-07-22 18:04:51 -0500

    TreeNode done

--- 2 Trees/TreeNode.js
+++ 2 Trees/TreeNode.js
@@ -17,17 +17,38 @@ class TreeNode {
     // To know that the target child has been removed from the children array
     const length = this.children.length
     this.children = this.children.filter(child => {
-      if (childToRemove instanceof TreeNode) {
-        return childToRemove !== child;
-      } else {
-        return child.data !== childToRemove;
-      }
+      return childToRemove instanceof TreeNode
+      ? child !== childToRemove
+      : child.data !== childToRemove;
     })
     if (length === this.children.length) {
       // Recursively call .removeChild() for each child in the children array
       this.children.forEach(child => child.removeChild(childToRemove))
     }
+  }
 
+  print(level = 0) {
+    let result = '';
+    for (let i = 0; i < level; i++) {
+      result += '-- ';
+    }
+    console.log(`${result}${this.data}`);
+    this.children.forEach(child => child.print(level + 1));
+  }
+
+  depthFirstTraversal() {
+    console.log(this.data)
+    this.children.forEach(child => child.depthFirstTraversal())
+  }
+
+  breadthFirstTraversal() {
+    // current node
+    let queue = [this]
+    while(queue.length > 0) {
+      const current = queue.shift()
+      console.log(current.data)
+      queue = queue.concat(current.children)
+    }
   }
 
 };
@@ -41,9 +62,39 @@ const tree = new TreeNode(1);
 tree.addChild(15);
 const node = new TreeNode(30);
 tree.addChild(node);
+tree.print()
 
-console.log('Displaying treeNode:', tree)
 tree.removeChild(15)
-console.log('After removing by data:', tree)
+tree.print()
 tree.removeChild(node)
-console.log('After removing by treeNode:', tree)
+tree.print()
+
+const randomize = () => Math.floor(Math.random() * 20);
+
+// add first-level children
+for (let i = 0; i < 3; i++) {
+  tree.addChild(randomize());
+}
+
+// add second-level children
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 2; j++) {
+    tree.children[i].addChild(randomize());
+  }
+}
+
+// add third-level children
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 2; j++) {
+    for (let k = 0; k < 2; k++) {
+      tree.children[i].children[j].addChild(randomize());
+    }
+  }
+}
+tree.print()
+tree.depthFirstTraversal()
+tree.print()
+tree.breadthFirstTraversal()
+
+
+
